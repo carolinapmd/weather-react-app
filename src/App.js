3294ddc9e1b62336c7cf4a9aa83ac61b44ab68e6
carolinapmd <71374980+carolinapmd@users.noreply.github.com>
2020-11-20 15:59:25 +0000

    Added the GitHub Url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">weather app</div>
+    <div className="App">
+      <h1>weather app</h1>
+    <footer>
+       💻 This project was coded by <a href="linkedin.com/in/carolina-domingues" target="_blank">Carolina Domingues</a>, and is open-sourced on{" "} 
+    <a href="https://github.com/carolinapmd/weather-react-app" target="_blank"> GitHub.</a>
+    </footer>
+    </div>
   );
 }
